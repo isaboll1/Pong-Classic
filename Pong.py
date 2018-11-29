8a4d926c896d49a30b7accc481ce37c1c0938396
isaboll1 <isaboll12@gmail.com>
2018-11-29 02:54:29 -0500

    Logic for the paddles, and ball bouncing. font change for readability.

--- Pong.py
+++ Pong.py
@@ -50,7 +50,7 @@ class TextObject:
     fonts = dict()
 
     def __init__(self, renderer, text, width, height,
-                font_name, color = (0, 0, 0), location = (0, 0), font_size = 36):
+                font_name, color = (0, 0, 0), location = (0, 0), font_size = 34):
         self.r = renderer
         if len(font_name) > 1:
             TextObject.fonts[font_name[0]] = TTF_OpenFont(font_name[1], font_size)
@@ -146,6 +146,63 @@ class Walls:
             SDL_RenderFillRect(self.r, wall)
 
 
+class Ball:
+    def __init__(self, renderer, position = (0,0), size = 20, color = (0, 0, 0)):
+        self.r = renderer
+        self.rect = SDL_Rect(position[0], position[1], size, size)
+        self.color = SDL_Color(color[0], color[1], color[2], 255)
+        self.x = position[0]
+        self.y = position[1]
+
+    def Set_Position(self, position):
+        self.x = self.rect.x = position[0]
+        self.y = self.rect.y = position[1]
+
+    def Is_Touching_Wall(self, wall):
+        for w in wall.bounds:
+            if SDL_HasIntersection(w, self.rect):
+                return True
+        return False
+
+    def Move(self, degree, speed = 5):
+        self.x += speed * cos(radians(degree))
+        self.y +=  speed * sin(radians(degree))
+        self.rect.x = int(self.x)
+        self.rect.y = int(self.y)
+
+    def Render(self):
+        SDL_SetRenderDrawColor(self.r, self.color.r, self.color.g, self.color.b, self.color.a)
+        SDL_RenderFillRect(self.r, self.rect)
+
+
+class Clock:
+    def __init__(self):
+        self.last_time = 0
+        self.current_time = SDL_GetPerformanceCounter()
+        self.dt = 0
+        self.dt_s = 0
+        self.s = 0
+        self.count = 0.0
+
+    def Tick(self):
+        self.last_time = self.current_time
+        self.current_time = SDL_GetPerformanceCounter()
+        self.dt = (self.current_time - self.last_time) * 1000 / SDL_GetPerformanceFrequency()
+        self.dt_s = self.dt * .001
+        if DEBUG:
+            print('DT SECONDS:', self.dt_s)
+            print('DT:', self.dt)
+
+    def Get_Distance(self, speed):
+        """ This function returns the amount of pixels to move by under a constant timestep,
+            no matter how fast the processor actually is on the host computer"""
+        return (speed * 100) * self.dt_s
+
+    def Counter(self):
+        self.s += self.dt_s
+
+
+
 # FUNCTIONS_____________________________________________________________________________________________________
 def WindowState(window, renderer, fs):
     if not fs:
@@ -160,6 +217,48 @@ def Deleter(dictionary_list):
     for dictionary in dictionary_list:
         for item in list(dictionary):
             del dictionary[item]
+
+
+def Change_Degree(paddles, degree, ball):
+    """ The result of the ball touching the paddle is saved under the result variable
+        from the result variable, the second value of the result variable is used to
+        check which angle to move the ball in. """
+    paddle_1_result = paddles[0].Is_Touching(ball)
+    paddle_2_result = paddles[1].Is_Touching(ball)
+    if paddle_1_result[0]:
+        ball.Set_Position(((int(ball.x) + 10), int(ball.y)))
+        if paddle_1_result[1] == 6:
+            return 60
+        elif paddle_1_result[1] == 5:
+            return 45
+        elif paddle_1_result[1] == 4:
+            return 30
+        elif paddle_1_result[1] == 3:
+            return 0
+        elif paddle_1_result[1] == 2:
+            return 330
+        elif paddle_1_result[1] == 1:
+            return 315
+        elif paddle_1_result[1] == 0:
+            return 300
+
+    if paddle_2_result[0]:
+        ball.Set_Position(((int(ball.x) - 10), int(ball.y)))
+        if paddle_2_result[1] == 6:
+            return 120
+        elif paddle_2_result[1] == 5:
+            return 135
+        elif paddle_2_result[1] == 4:
+            return 150
+        elif paddle_2_result[1] == 3:
+            return 180
+        elif paddle_2_result[1] == 2:
+            return 210
+        elif paddle_2_result[1] == 1:
+            return 225
+        elif paddle_2_result[1] == 0:
+            return 240
+    return degree
 
 
 # MAIN__________________________________________________________________________________________________________
@@ -183,22 +282,28 @@ def main():
     game = False
     fullscreen = False
     paused = False
-    speed = 5
+    speed = 6
+    ball_speed = 8
+    degree = 180
 
     # Objects___________________________________________________________________________________________________
     mouse = Pointer()
     menu_items = {
-    'Title':       TextObject(renderer, 'Pong   Classic', 600, 300, ['arcade', b'font/arcade.ttf'], location = (345, 0)),
-    'Start':       TextObject(renderer, 'Start', 100, 100, ['arcade'], location = (583, 350)),
-    'Fullscreen':  TextObject(renderer, 'Fullscreen', 200, 85, ['arcade'], location = (543, 454)),
-    'Quit':        TextObject(renderer, 'Quit', 80, 100, ['arcade'], location = (590, 550))
+    'Title':       TextObject(renderer, 'Pong Classic', 600, 240, ['joystix', b'font/joystix.ttf'], location = (345, 0)),
+    'Start':       TextObject(renderer, 'Start', 90, 85, ['joystix'], location = (583, 350)),
+    'Fullscreen':  TextObject(renderer, 'Fullscreen', 200, 85, ['joystix'], location = (543, 450)),
+    'Quit':        TextObject(renderer, 'Quit', 90, 85, ['joystix'], location = (590, 550))
     }
     paddles = [Paddle(renderer, position = (20, 290)), Paddle(renderer, position = (1245, 290))]
     wall = Walls(renderer)
+    ball = Ball(renderer, position = (60, 290))
+    clock = Clock()
 
     # Game Loop_________________________________________________________________________________________________
     while (running):
         keystate = SDL_GetKeyboardState(None)
+        clock.Tick()
+
         # Event Loop___________________________________________________________
         while(SDL_PollEvent(ctypes.byref(event))):
             mouse.Compute(event)
@@ -210,6 +315,12 @@ def main():
             break
 
         # Logic________________________________________________________________
+        degree = Change_Degree(paddles, degree, ball)
+        if not paused:
+            if ball.Is_Touching_Wall(wall):
+                degree = 360-degree
+            ball.Move(degree, ball_speed)
+
         if (menu):
             for item in menu_items:
                 if item == 'Title':
@@ -258,13 +369,25 @@ def main():
                 if keystate[SDL_SCANCODE_DOWN]:
                     paddles[1].Move('DOWN', speed)
 
+            """ This code is for making sure the ball goes back to the middle 
+                after going outta bounds """
+            if ball.x < -20 or ball.x > WIDTH + 20:
+                ball_speed = 0
+                clock.Counter()
+                if clock.s > 2:
+                    clock.s = 0
+                    ball.Set_Position((WIDTH // 2, HEIGHT // 2))
+                    ball_speed = 7
+
+
         # Rendering____________________________________________________________
         SDL_SetRenderDrawColor(renderer, 252, 252, 252, 255)
         SDL_RenderClear(renderer)
+
         for paddle in paddles:
             paddle.Render()
-
         wall.Render()
+        ball.Render()
 
         if (menu):
             for item in menu_items:
